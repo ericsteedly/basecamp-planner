4c4f9cd6176e0160f8135dd659cc3dd269b9c9ec
Eric Steedly <ericsteedly@gmail.com>
2024-01-26 13:58:24 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      Working
+    </div>
+    // <Routes>
+    //   <Route></Route>
+    // </Routes>
+  );
+}
+
+export default App;
